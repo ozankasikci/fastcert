d4afd85b73288d9751808eace77dfa6bdff6d8a9
Ozan Kaşıkçı <ozan@kasikci.io>
2025-12-10 02:15:33 +0300

    add help text and usage

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
 use clap::Parser;
 use rscert::Result;
 
+const AFTER_HELP: &str = "\
+EXAMPLES:
+    $ rscert -install
+    Install the local CA in the system trust store.
+
+    $ rscert example.org
+    Generate \"example.org.pem\" and \"example.org-key.pem\".
+
+    $ rscert example.com myapp.dev localhost 127.0.0.1 ::1
+    Generate \"example.com+4.pem\" and \"example.com+4-key.pem\".
+
+    $ rscert \"*.example.it\"
+    Generate \"_wildcard.example.it.pem\" and \"_wildcard.example.it-key.pem\".
+
+    $ rscert -uninstall
+    Uninstall the local CA (but do not delete it).
+
+ENVIRONMENT:
+    CAROOT
+        Set the CA certificate and key storage location. (This allows
+        maintaining multiple local CAs in parallel.)
+
+    TRUST_STORES
+        A comma-separated list of trust stores to install the local
+        root CA into. Options are: \"system\", \"java\" and \"nss\" (includes
+        Firefox). Autodetected by default.
+";
+
 #[derive(Parser, Debug)]
 #[command(name = "rscert")]
 #[command(version)]
-#[command(about = "A simple zero-config tool to make locally-trusted development certificates", long_about = None)]
+#[command(about = "A simple zero-config tool to make locally-trusted development certificates")]
+#[command(after_help = AFTER_HELP)]
 struct Cli {
     /// Install the local CA in the system trust store
     #[arg(long)]
