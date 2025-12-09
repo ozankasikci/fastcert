a642d614821478fde967b383ae037dc85b7ebed7
Ozan Kaşıkçı <ozan@kasikci.io>
2025-12-10 02:30:56 +0300

    add Windows certificate store API

--- src/truststore/windows.rs
+++ src/truststore/windows.rs
@@ -2,8 +2,32 @@
 
 use crate::Result;
 use super::TrustStore;
+use std::path::Path;
 
-pub struct WindowsTrustStore;
+#[cfg(target_os = "windows")]
+use {
+    crate::Error,
+    std::ptr,
+    windows::Win32::Security::Cryptography::{
+        CertAddEncodedCertificateToStore, CertCloseStore, CertDeleteCertificateFromStore,
+        CertDuplicateCertificateContext, CertEnumCertificatesInStore, CertOpenSystemStoreW,
+        CERT_CONTEXT, CERT_STORE_ADD_REPLACE_EXISTING, CERT_STORE_PROV_SYSTEM_W,
+        HCERTSTORE, PKCS_7_ASN_ENCODING, X509_ASN_ENCODING,
+    },
+    windows::core::PCWSTR,
+};
+
+pub struct WindowsTrustStore {
+    cert_path: String,
+}
+
+impl WindowsTrustStore {
+    pub fn new(cert_path: &Path) -> Self {
+        Self {
+            cert_path: cert_path.to_string_lossy().to_string(),
+        }
+    }
+}
 
 impl TrustStore for WindowsTrustStore {
     fn check(&self) -> Result<bool> {
