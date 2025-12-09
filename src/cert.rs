1471d7f2f63c80e9a2afc53924612d09c7dd04f2
Ozan Kaşıkçı <ozan@kasikci.io>
2025-12-10 01:52:09 +0300

    expand certificate configuration

--- src/cert.rs
+++ src/cert.rs
@@ -1,12 +1,17 @@
 //! Certificate generation
 
-use crate::Result;
+use crate::{Error, Result};
+use std::net::IpAddr;
+use std::path::PathBuf;
 
 pub struct CertificateConfig {
     pub hosts: Vec<String>,
     pub use_ecdsa: bool,
     pub client_cert: bool,
     pub pkcs12: bool,
+    pub cert_file: Option<PathBuf>,
+    pub key_file: Option<PathBuf>,
+    pub p12_file: Option<PathBuf>,
 }
 
 impl CertificateConfig {
@@ -16,6 +21,9 @@ impl CertificateConfig {
             use_ecdsa: false,
             client_cert: false,
             pkcs12: false,
+            cert_file: None,
+            key_file: None,
+            p12_file: None,
         }
     }
 }
