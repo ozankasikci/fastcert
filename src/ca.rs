9810e87a1cf5955be50ad272d59e1dbd82ef6554
Ozan Kaşıkçı <ozan@kasikci.io>
2025-12-10 01:44:16 +0300

    add CA file name constants

--- src/ca.rs
+++ src/ca.rs
@@ -3,6 +3,9 @@
 use crate::{Error, Result};
 use std::path::PathBuf;
 
+const ROOT_CERT_FILE: &str = "rootCA.pem";
+const ROOT_KEY_FILE: &str = "rootCA-key.pem";
+
 pub struct CertificateAuthority {
     root_path: PathBuf,
 }
