f58969e5238cff8dd812415c1b4d0051baa31c92
Ozan Kaşıkçı <ozan@kasikci.io>
2025-12-10 01:44:33 +0300

    expand CA structure with certificate fields

--- src/ca.rs
+++ src/ca.rs
@@ -1,17 +1,28 @@
 //! Certificate Authority management
 
 use crate::{Error, Result};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use rcgen::Certificate;
 
 const ROOT_CERT_FILE: &str = "rootCA.pem";
 const ROOT_KEY_FILE: &str = "rootCA-key.pem";
 
 pub struct CertificateAuthority {
     root_path: PathBuf,
+    cert: Option<Certificate>,
+    cert_pem: Option<String>,
 }
 
 impl CertificateAuthority {
     pub fn new(root_path: PathBuf) -> Self {
-        Self { root_path }
+        Self {
+            root_path,
+            cert: None,
+            cert_pem: None,
+        }
+    }
+
+    pub fn root_path(&self) -> &Path {
+        &self.root_path
     }
 }
